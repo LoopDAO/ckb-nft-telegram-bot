e745bd95cad8016f73a80213c2509060c7454037
akun.ubuntu2004 <20220428.bzly>
2022-05-20 11:10:43 +0800

    feat: add https_proxy_agent

--- src/handlers/registerStartMenu.js
+++ src/handlers/registerStartMenu.js
@@ -8,100 +8,101 @@ const mainnetURL = 'https://flashsigner.com'
 const testnetURL = 'https://staging.flashsigner.work'
 
 exports.registerStartMenu = async (bot) => {
-  bot.start(async (ctx) => {
-    const chainType = process.env.CHAIN_TYPE || 'testnet'
-    const network = chainType === 'mainnet' ? mainnetURL : testnetURL
-    console.log('network...', network)
-    Config.setFlashsignerURL(network)
-    Config.setChainType(chainType)
+    bot.start(async (ctx) => {
+        const chainType = process.env.CHAIN_TYPE || 'testnet'
+        const network = chainType === 'mainnet' ? mainnetURL : testnetURL
+        console.log('network...', network)
+        Config.setFlashsignerURL(network)
+        Config.setChainType(chainType)
 
-    console.log('ctx.update...', ctx.update.message)
-    const user = ctx.user
-    const chat = ctx.chat
-    const startPayload = ctx.startPayload
+        console.log('ctx.update...', ctx.update.message)
+        const user = ctx.user
+        const chat = ctx.chat
+        const startPayload = ctx.startPayload
 
-    if (chat.type === 'private') {
-      if (startPayload && startPayload !== 'c') {
-        const group = await getGroupInfo(startPayload)
-        if (group) {
-          const sender = ctx.from
-          const data = {
-            userId: sender.id,
-            firstName: sender?.first_name,
-            lastName: sender?.last_name,
-            isBot: sender?.is_bot,
-            groupName: group.groupName,
-            groupId: group.groupId
-          }
-          const successURL = `${process.env.SERVER_URL}/api/wallet`
-          const token = jwt.sign(data, process.env.TOKEN_SECRET)
+        if (chat.type === 'private') {
+            if (startPayload && startPayload !== 'c') {
+                console.log('startPayload...', startPayload)
+                const group = await getGroupInfo(startPayload)
+                if (group) {
+                    const sender = ctx.from
+                    const data = {
+                        userId: sender.id,
+                        firstName: sender?.first_name,
+                        lastName: sender?.last_name,
+                        isBot: sender?.is_bot,
+                        groupName: group.groupName,
+                        groupId: group.groupId
+                    }
+                    const successURL = `${process.env.SERVER_URL}/api/wallet`
+                    const token = jwt.sign(data, process.env.TOKEN_SECRET)
 
-          const url = generateSignMessageURL(successURL, {
-            message: token,
-            isRaw: true
-          })
-          console.log('generate url...', url)
-          return ctx.reply(
-            `${group.groupName} is NFT holders chat room.`,
-            Markup.inlineKeyboard([Markup.button.url(`Connect`, url)])
-          )
+                    const url = generateSignMessageURL(successURL, {
+                        message: token,
+                        isRaw: true
+                    })
+                    console.log('generate url...', url)
+                    return ctx.reply(
+                        `${group.groupName} is NFT holders chat room.`,
+                        Markup.inlineKeyboard([Markup.button.url(`Connect`, url)])
+                    )
+                }
+            }
+
+            const username = ctx.from.username
+            let message = `<b>Welcome to ${process.env.BOT_NAME} ${username}</b>! I am your bot. I am here to help manage your groups and NFT holders. Choose below to get started.`
+            let inlineButtons = []
+            if (user?.groups?.length > 0) {
+                inlineButtons = [Markup.button.callback(`🍃 Group Admin`, 'groupAdmin')]
+            } else {
+                inlineButtons = [
+                    Markup.button.callback(`🍋 Setup NFT Holders Group`, 'setup')
+                ]
+            }
+            return await ctx.replyWithAnimation(
+                { source: fs.readFileSync('./src/assets/robot.gif') },
+                {
+                    caption: message,
+                    parse_mode: 'HTML',
+                    ...Markup.inlineKeyboard(inlineButtons)
+                }
+            )
         }
-      }
 
-      const username = ctx.from.username
-      let message = `<b>Welcome to ${process.env.BOT_NAME} ${username}</b>! I am your bot. I am here to help manage your groups and NFT holders. Choose below to get started.`
-      let inlineButtons = []
-      if (user?.groups?.length > 0) {
-        inlineButtons = [Markup.button.callback(`🍃 Group Admin`, 'groupAdmin')]
-      } else {
-        inlineButtons = [
-          Markup.button.callback(`🍋 Setup NFT Holders Group`, 'setup')
-        ]
-      }
-      return await ctx.replyWithAnimation(
-        { source: fs.readFileSync('./src/assets/robot.gif') },
-        {
-          caption: message,
-          parse_mode: 'HTML',
-          ...Markup.inlineKeyboard(inlineButtons)
+        if (startPayload === 'c') {
+            const message = `Thank you for adding me to the group. Please make sure to promote me as an administrator.`
+            console.log('add to group ctx...', ctx.update.message.chat)
+
+            await ctx.reply(message)
+            await ctx.telegram.sendMessage(
+                ctx.from.id,
+                message,
+                Markup.inlineKeyboard([
+                    Markup.button.callback('🌸 Config NFT Holders Group', 'config')
+                ])
+            )
         }
-      )
-    }
+    })
+    bot.action('groupAdmin', groupAdmin)
 
-    if (startPayload === 'c') {
-      const message = `Thank you for adding me to the group. Please make sure to promote me as an administrator.`
-      console.log('add to group ctx...', ctx.update.message.chat)
-
-      await ctx.reply(message)
-      await ctx.telegram.sendMessage(
-        ctx.from.id,
-        message,
-        Markup.inlineKeyboard([
-          Markup.button.callback('🌸 Config NFT Holders Group', 'config')
+    async function groupAdmin(ctx) {
+        const groupList = ctx.user.groups.map((el) => [
+            Markup.button.callback(
+                `🍏 ${el.groupName}`,
+                `groups::${el.groupId}::${el.groupName}`
+            )
         ])
-      )
+        await ctx.reply(
+            `Please add me to the group as admin. Once added I'll help you to setup NFT holders chat room.`,
+            Markup.inlineKeyboard([
+                ...groupList,
+                [
+                    Markup.button.url(
+                        `Add ${process.env.BOT_NAME} to Group`,
+                        `https://t.me/${process.env.BOT_USER_NAME}?startgroup=c`
+                    )
+                ]
+            ])
+        )
     }
-  })
-  bot.action('groupAdmin', groupAdmin)
-
-  async function groupAdmin(ctx) {
-    const groupList = ctx.user.groups.map((el) => [
-      Markup.button.callback(
-        `🍏 ${el.groupName}`,
-        `groups::${el.groupId}::${el.groupName}`
-      )
-    ])
-    await ctx.reply(
-      `Please add me to the group as admin. Once added I'll help you to setup NFT holders chat room.`,
-      Markup.inlineKeyboard([
-        ...groupList,
-        [
-          Markup.button.url(
-            `Add ${process.env.BOT_NAME} to Group`,
-            `https://t.me/${process.env.BOT_USER_NAME}?startgroup=c`
-          )
-        ]
-      ])
-    )
-  }
 }
--- src/index.js
+++ src/index.js
@@ -13,45 +13,60 @@ const cota = require('./service/cotaService')
 
 const token = process.env.BOT_TOKEN
 if (token === undefined) {
-  throw new Error('BOT_TOKEN must be provided!')
+    throw new Error('BOT_TOKEN must be provided!')
 }
-
-const bot = new Telegraf(token)
+const HttpsProxyAgent = require('https-proxy-agent');
+const proxyUrl = process.env.HTTPS_PROXY
+const agent = proxyUrl ? new HttpsProxyAgent(proxyUrl) : undefined
+const bot = new Telegraf(token, {
+    telegram: {
+        agent: agent
+    }
+})
 console.log('bot...', bot)
+
+//bot.start((ctx) => ctx.reply('Welcome'))
+// bot.help((ctx) => ctx.reply('Send me a sticker'))
+// bot.on('sticker', (ctx) => ctx.reply('👍'))
+// bot.hears('hi', (ctx) => ctx.reply('Hey there'))
+// bot.launch()
+
 const secretPath = `/telegraf/${bot.secretPathComponent()}`
 // Set telegram webhook
 bot.telegram.setWebhook(`${process.env.SERVER_URL}${secretPath}`)
+console.log('process.env.SERVER_URL...', process.env.SERVER_URL)
+console.log('secretPath...', process.env.secretPath)
 
 async function mainService() {
-  mongoose.connect(
-    process.env.DB_URL,
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    },
-    (err) => {
-      if (err) {
-        console.error(err.message)
-        console.error(err)
-      } else {
-        console.log('Connected to MongoDB')
-      }
-    }
-  )
-  bot.use(attachUser)
-  // register all bot commands
-  await registerHandlers(bot)
+    mongoose.connect(
+        process.env.DB_URL,
+        {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        },
+        (err) => {
+            if (err) {
+                console.error(err.message)
+                console.error(err)
+            } else {
+                console.log('Connected to MongoDB')
+            }
+        }
+    )
+    bot.use(attachUser)
+    // register all bot commands
+    await registerHandlers(bot)
 
-  // cronjob to update discord roles once a day
-  cron.schedule('0 0 * * *', () => {
-    cota.banGroupMembers(bot)
-  })
+    // cronjob to update discord roles once a day
+    cron.schedule('0 0 * * *', () => {
+        cota.banGroupMembers(bot)
+    })
 }
 mainService()
 
 // register global error handler to prevent the bot from stopping after an exception
 bot.catch((err, ctx) => {
-  console.error(`Ooops, encountered an error for ${ctx.updateType}`, err)
+    console.error(`Ooops, encountered an error for ${ctx.updateType}`, err)
 })
 
 const app = express()
@@ -61,78 +76,83 @@ app.use(bodyParser.json())
 app.use(bot.webhookCallback(secretPath))
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    res.send('Hello World!')
 })
 
 // deal with callback data when user connected wallet
 app.get('/api/wallet', async (req, res) => {
-  const { flashsigner_data } = req.query
-  const data = JSON.parse(flashsigner_data)
-  if (data.code === 200) {
-    console.log('data.result', data.result)
-    try {
-      const { lock, message, sig } = data.result
-      console.log('lock...', lock)
-
-      const isValidSig = validateSignature(message, sig)
-      if (!isValidSig) {
-        return res.send('Your signature is not valid!')
-      }
-
-      // TODO: use testnet address
-      let address = getWalletAddress(sig)
-      console.log('address....', address)
-      const decoded = jwt.verify(message, process.env.TOKEN_SECRET)
-      const { userId, groupName, groupId } = decoded
-      /*
-   send below message to a user who wanna join when bot is checking if the user's address has required nfts. Here will use ckb api to do the job
-  */
-      await bot.telegram.sendMessage(userId, 'Processing!!! Please wait...')
-
-      const cotaCount = await cota.isQualified(address, groupId)
-      if (cotaCount > 0) {
-        await saveMemberInfo({ ...decoded, walletAddress: address })
-        // send below message if a user is approved to join group
-        try {
-          // check if the user is a member of this group
-          await bot.telegram.getChatMember(groupId, userId)
-        } catch (err) {
-          if (
-            err.response &&
-            err.response.description === 'Bad Request: user not found'
-          ) {
-            try {
-              await bot.telegram.approveChatJoinRequest(groupId, userId)
-            } catch (err) {
-              await bot.telegram.sendMessage(
-                userId,
-                `Sorry, you could not join ${groupName}`
-              )
-              return res.send(`Sorry, you could not join ${groupName}`)
-            }
-          }
-        }
-        const chat = await bot.telegram.getChat(groupId)
-        console.log('chat...', chat)
-        await bot.telegram.sendMessage(userId, `Welcome to ${groupName}`, {
-          ...Markup.inlineKeyboard([
-            Markup.button.url(`Join Group`, `${chat.invite_link}`)
-          ])
-        })
-        return res.send(`Welcome to ${groupName}`)
-      } else {
-        await bot.telegram.sendMessage(
-          userId,
-          `Sorry, you could not join ${groupName}`
-        )
-        return res.send(`Sorry, you could not join ${groupName}`)
-      }
-    } catch (err) {
-      console.log('verify message err...', err)
+    const { flashsigner_data } = req.query
+    if (!flashsigner_data) {
+        console.log('flashsigner_data is undefined')
+        return res.status(400).send('Missing flashsigner_data')
     }
-  }
+    console.log("flashsigner_data...", flashsigner_data)
+    const data = JSON.parse(flashsigner_data)
+    if (data.code === 200) {
+        console.log('data.result', data.result)
+        try {
+            const { lock, message, sig } = data.result
+            console.log('lock...', lock)
+
+            const isValidSig = validateSignature(message, sig)
+            if (!isValidSig) {
+                return res.send('Your signature is not valid!')
+            }
+
+            // TODO: use testnet address
+            let address = getWalletAddress(sig)
+            console.log('address....', address)
+            const decoded = jwt.verify(message, process.env.TOKEN_SECRET)
+            const { userId, groupName, groupId } = decoded
+            /*
+         send below message to a user who wanna join when bot is checking if the user's address has required nfts. Here will use ckb api to do the job
+        */
+            await bot.telegram.sendMessage(userId, 'Processing!!! Please wait...')
+
+            const cotaCount = await cota.isQualified(address, groupId)
+            if (cotaCount > 0) {
+                await saveMemberInfo({ ...decoded, walletAddress: address })
+                // send below message if a user is approved to join group
+                try {
+                    // check if the user is a member of this group
+                    await bot.telegram.getChatMember(groupId, userId)
+                } catch (err) {
+                    if (
+                        err.response &&
+                        err.response.description === 'Bad Request: user not found'
+                    ) {
+                        try {
+                            await bot.telegram.approveChatJoinRequest(groupId, userId)
+                        } catch (err) {
+                            await bot.telegram.sendMessage(
+                                userId,
+                                `Sorry, you could not join ${groupName}`
+                            )
+                            return res.send(`Sorry, you could not join ${groupName}`)
+                        }
+                    }
+                }
+                const chat = await bot.telegram.getChat(groupId)
+                console.log('chat...', chat)
+                await bot.telegram.sendMessage(userId, `Welcome to ${groupName}`, {
+                    ...Markup.inlineKeyboard([
+                        Markup.button.url(`Join Group`, `${chat.invite_link}`)
+                    ])
+                })
+                return res.send(`Welcome to ${groupName}`)
+            } else {
+                await bot.telegram.sendMessage(
+                    userId,
+                    `Sorry, you could not join ${groupName}`
+                )
+                return res.send(`Sorry, you could not join ${groupName}`)
+            }
+        } catch (err) {
+            console.log('verify message err...', err)
+        }
+    }
 })
 
 app.listen(3000, () => {
-  console.log('Example app listening on port 3000!')
+    console.log('Example app listening on port 3000!')
 })
